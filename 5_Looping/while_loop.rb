1c1209552cda6dba1a89c7a3440dbc0f7e693585
Kushal Mondal <68580569+Kushalmydesk@users.noreply.github.com>
2023-07-03 13:51:16 +0530

    Update while_loop.rb

--- 5_Looping/while_loop.rb
+++ 5_Looping/while_loop.rb
@@ -1,4 +1,5 @@
 # While loop is a simple Entry Control Loop
+# We need while loop where the looping time is unknown
 
 
 index = 1
@@ -8,7 +9,18 @@ while index <= 5
     index += 1
 end
 
+=begin
+    In Ruby, the until keyword is used to create a loop that executes a block of code repeatedly until a certain condition becomes true. 
+    It is the opposite of the while loop, which executes the block of code while a condition is true.
+=end
 
+
+counter = 1
+until counter > 10
+  puts counter
+  # Add code to update 'counter' here!
+  counter += 1
+end
 
 
 
